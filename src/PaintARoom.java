a8ea8ff49058498a2e4746dfa478e57dd77ce2db
Drew Rautenberg <drew@drewrautenberg.com>
2017-09-28 12:04:43 -0400

    formatted code

--- src/PaintARoom.java
+++ src/PaintARoom.java
@@ -42,35 +42,35 @@ public class PaintARoom {
         winDoorArea = findWinDoorArea(AVE_DOOR_AREA, AVE_WINDOW_AREA);
         paintableArea = totalArea - winDoorArea;
 
-        // NOTE:  Uncomment next line after you've written the method        
-        gallons = findGallonsNeeded(paintableArea,NUM_COATS,SQ_FT_PER_GALLON);
+        gallons = findGallonsNeeded(paintableArea, NUM_COATS, SQ_FT_PER_GALLON);
 
         //-------------------------------------------------------------- OUTPUT
-        // NOTE:  Uncomment next line once gallons has a value
         System.out.println("Need " + gallons + " gallons of paint for " +
-              NUM_COATS + " coats");
+                NUM_COATS + " coats");
 
         //---------------------------------------------------------------------
         System.out.println("\nTHE END");
         System.exit(0);
     } // END OF main
+
     //*************************************************************************
     // This method returns the room's total wall area (in square feet)
     //      without considering windows or doors.
-    private static int findWallArea(){
+    private static int findWallArea() {
         String userString = JOptionPane.showInputDialog("Enter room height (8 is normal)?");
         int height = Integer.parseInt(userString);
         int length = Integer.parseInt(JOptionPane.showInputDialog("Enter length of room (a whole number)?"));
         int width = Integer.parseInt(JOptionPane.showInputDialog("Enter width of room (a whole number)?"));
 
         int area;
-        area = 2*((length*height)+(width*height));
+        area = 2 * ((length * height) + (width * height));
         return area;
     }
+
     //*************************************************************************
     // This method finds the room's total square footage for all the doors
     //      and windows (based on aveage sizes for doors/windows).
-    private static int findWinDoorArea(int aveDoorArea, int avewindowArea){
+    private static int findWinDoorArea(int aveDoorArea, int avewindowArea) {
         int numDoors = Integer.parseInt(JOptionPane.showInputDialog
                 ("Enter number of doors"));
         int numWindows = Integer.parseInt(JOptionPane.showInputDialog
@@ -79,34 +79,29 @@ public class PaintARoom {
         int doorArea;
         int winArea;
         int total;
-        doorArea = numDoors*aveDoorArea;
-        winArea=numWindows*avewindowArea;
-        total = doorArea+winArea;
-        return total ;
+        doorArea = numDoors * aveDoorArea;
+        winArea = numWindows * avewindowArea;
+        total = doorArea + winArea;
+        return total;
     }
+
     //*************************************************************************
     // This method finds the total number of gallons needed to paint the given
     // room (based on data user provided and constant values assumed in this
     // program
-    private static int findGallonsNeeded(int area,int coats,int sqFtperGallon){
+    private static int findGallonsNeeded(int area, int coats, int sqFtperGallon) {
         int totalGallons;
         int areaWithCoats;
         double numGallons;
 
-        areaWithCoats = area*coats;
-        numGallons=areaWithCoats/sqFtperGallon;
+        areaWithCoats = area * coats;
+        numGallons = areaWithCoats / sqFtperGallon;
 
-        if (numGallons%1==0){
-            totalGallons=(int)numGallons;
-        }
-        else {
-            totalGallons=(int)numGallons+1;
+        if (numGallons % 1 == 0) {
+            totalGallons = (int) numGallons;
+        } else {
+            totalGallons = (int) numGallons + 1;
         }
         return totalGallons;
     }
-
-    // HINT:  If there's a remainder when dividing by SQ_FT_PER_GALLON, then
-    //      add another gallon since you can't buy a partial gallon
-
-    //*************************************************************************
 } // END OF CLASS (the whole program)
